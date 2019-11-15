8d57a6864104d8eace887bede7ef7624307896a0
visupatlolla <visu_patlolla@yahoo.com>
2019-11-15 09:41:24 -0500

    Added the default execution in the calculator

--- calculator.py
+++ calculator.py
@@ -9,3 +9,8 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(subtract(3, 1))
